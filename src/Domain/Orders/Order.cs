044844e77ae376a1ecaffa51bbe5912af8dffd8a
Chris van Tiggelen <chrisvt@gitkraken.nl>
2024-12-12 20:22:52 +0100

    WIP feature  'order management'

--- src/Domain/Orders/Order.cs
+++ src/Domain/Orders/Order.cs
@@ -1,4 +1,4 @@
-﻿using Domain.Products;
+﻿using Domain.Users;
 using SharedKernel;
 
 namespace Domain.Orders;
@@ -7,5 +7,7 @@ public sealed class Order : Entity
 {
     public Guid Id { get; set; }
     public Guid UserId { get; set; }
-    public List<Product> Products { get; set; } // subject to change to list of order items
+    public User user { get; set; }
+    public decimal TotalPrice { get; set; }
+    public List<OrderItem> OrderItems { get; set; } = []; // subject to change to list of order items
 }
---
+++ src/Domain/Orders/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Products;
+using SharedKernel;
+
+namespace Domain.Orders;
+
+public sealed class OrderItem : Entity
+{
+    public Guid Id { get; set; }
+    public Guid OrderId { get; set; }
+    public Order order { get; set; }
+    public Guid ProductId { get; set; }
+    public Product Product { get; set; }
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal TotalPrice => Quantity * UnitPrice;
+}
